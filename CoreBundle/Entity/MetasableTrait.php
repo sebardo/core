a85102f3a0849968d1d2d2a1302ce4af77c406dd
sebardo <sebastian.sasturain@gmail.com>
2017-04-18 09:08:22 +0200

    Change controller on generator command

---
+++ CoreBundle/Entity/MetasableTrait.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace CoreBundle\Entity;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * MetasableTrait traiut class to define metas
+ *
+ */
+trait MetasableTrait 
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="meta_title", type="string", length=255, nullable=true)
+     */
+    private $metaTitle;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="meta_description", type="text", nullable=true)
+     */
+    private $metaDescription;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="meta_tags", type="string", length=255, nullable=true)
+     */
+    private $metaTags;
+    
+    
+    /**
+     * Get metaTitle
+     *
+     * @return string
+     */
+    public function getMetaTitle()
+    {
+        return $this->metaTitle;
+    }
+
+    /**
+     * Set metaTitle
+     *
+     * @param string $metaTitle
+     *
+     * @return Product
+     */
+    public function setMetaTitle($metaTitle)
+    {
+        $this->metaTitle = $metaTitle;
+
+        return $this;
+    }
+
+    /**
+     * Get metaDescription
+     *
+     * @return string
+     */
+    public function getMetaDescription()
+    {
+        return $this->metaDescription;
+    }
+    
+    /**
+     * Set metaDescription
+     *
+     * @param string $metaDescription
+     *
+     * @return Product
+     */
+    public function setMetaDescription($metaDescription)
+    {
+        $this->metaDescription = $metaDescription;
+
+        return $this;
+    }
+
+    /**
+     * Get metaTags
+     *
+     * @return string
+     */
+    public function getMetaTags()
+    {
+        return $this->metaTags;
+    }
+
+    /**
+     * Set metaTags
+     *
+     * @param string $metaTags
+     *
+     * @return Product
+     */
+    public function setMetaTags($metaTags)
+    {
+        $this->metaTags = $metaTags;
+
+        return $this;
+    }
+}
